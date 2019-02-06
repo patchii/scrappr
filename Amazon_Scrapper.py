d194c1ff888e6e82fbdb4c920c90bb2d50f051b3
patchii <mehdi.massoudi@ensi-uma.tn>
2019-02-06 19:30:31 +0100

    added version1 of scrapper

---
+++ Amazon_Scrapper.py
@@ -0,0 +1,31 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+my_url = 'https://www.amazon.com/Huawei-Mate-LYA-L29-128GB-International/product-reviews/B07JZ64BWC/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews'
+#opent connex an grab  
+uClient = uReq(my_url)
+page_html= uClient.read()
+uClient.close()
+page_soup=soup(page_html,"html.parser")
+containers = page_soup.findAll("div",{"class": "a-section celwidget"})
+
+filename="review.csv"
+f= open(filename,"w")
+headers="title, rating, review\n"
+f.write(headers)
+
+for container in containers:    
+    
+    title_container=container.findAll("a",{"class":"a-size-base a-link-normal review-title a-color-base a-text-bold"})
+    review_title=title_container[0].text
+    
+    rating_container=container.findAll("a",{"class":"a-link-normal"})
+    rating=rating_container[0]["title"]
+    
+    review_container=container.findAll("span",{"class":"a-size-base review-text"})
+    review=review_container[0].text
+    
+    
+    
+    f.write(review_title.replace(",","|") + "," + rating + "," + review.replace(",","|") + "\n")
+
+f.close()
